985d5c5b379956ed2d9ac4d72fa5b04e074ff7a5
mrpetrov <mrpetrov@users.noreply.github.com>
2020-10-29 23:00:57 +0200

    update-executable: new version of script

--- scripts/update-hpm-executable.sh
+++ scripts/update-hpm-executable.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-service_name=hpm
-src_dir=/home/pi/$service_name
 
-service $service_name stop
-cp $src_dir/$service_name /usr/sbin
-chown root:root /usr/sbin/$service_name
-chmod a+x /usr/sbin/$service_name
-sleep 6
-service $service_name start
+daemon=hpm
+daemon_pid=/run/$daemon.pid
+log_file=/var/log/$daemon.log
 
+echo "Trying to kill running daemon $daemon..."
+if [ ! -e $daemon_pid ]
+then
+    killall -e $daemon
+else
+    kill `cat $daemon_pid`
+    rm -f $daemon_pid
+fi
+sleep 1
+echo "Replacing /usr/sbin/$daemon with the one from /home/pi/$daemon..."
+cp /home/pi/$daemon/$daemon /usr/sbin
+sleep 1
+echo "Starting $daemon again..."
+/usr/sbin/$daemon
+echo "Here is the log:"
+tail -n 25 $log_file
