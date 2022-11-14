0727696fed7b58c1dc934df404e4202608a88ce3
mrpetrov <mrpetrov@users.noreply.github.com>
2022-11-14 15:40:58 +0200

    update scripts files, add stop and backup scripts

--- scripts/update-hpm-executable.sh
+++ scripts/update-hpm-executable.sh
@@ -2,7 +2,7 @@
 
 daemon=hpm
 daemon_pid=/run/$daemon.pid
-log_file=/var/log/$daemon.log
+log_file=/run/shm/$daemon.log
 
 echo "Trying to kill running daemon $daemon..."
 if [ ! -e $daemon_pid ]
@@ -13,13 +13,18 @@ else
     rm -f $daemon_pid
 fi
 sleep 1
+/etc/cron.hourly/move-hpm-log
 echo "Replacing /usr/sbin/$daemon with the one from /home/pi/$daemon..."
 cp /home/pi/$daemon/$daemon /usr/sbin
 echo "Replacing $daemon-reload and $daemon-restart in /usr/sbin with ones from /home/pi/$daemon/scripts/..."
 cp /home/pi/$daemon/scripts/$daemon-reload /usr/sbin
 cp /home/pi/$daemon/scripts/$daemon-restart /usr/sbin
+cp /home/pi/$daemon/scripts/$daemon-stop /usr/sbin
+cp /home/pi/$daemon/scripts/$daemon_backup-cfg /usr/sbin
 chmod +x /usr/sbin/$daemon-reload
 chmod +x /usr/sbin/$daemon-restart
+chmod +x /usr/sbin/$daemon-stop
+chmod +x /usr/sbin/$daemon_backup-cfg
 sleep 1
 echo "Starting $daemon again..."
 /usr/sbin/$daemon
